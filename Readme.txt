b5f4316806ae9b49a686de50a45bdc8e3134c5e8
sam stone <samstone191998@gmail.com>
2020-09-17 20:07:59 +0100

    Functionality Changes
    - Allowed the user to press ENTER to send messages rather then having to click submit.
    - Gave the user the option whether they want to output message logs to the console or not

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Things I want to add:
+    - Require the user to enter a username before chatting
+    - Creating seperate rooms where a user must have the room code or create a room
+
+
--- server.py
+++ server.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
+
 app = Flask(__name__, template_folder='./templates', static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
+app.config['MESSAGE_LOG'] = False # Turn on to print message logs to the console
 socketio = SocketIO(app)
 
 @app.route('/')
-def home():
+def chat_room():
     return render_template('index.html')
+
+
 
 @socketio.on('message')
 def handle_message(message):
-    print('received message: ' + message)
+    if app.config['MESSAGE_LOG']:
+        print('received message: ' + message)
     send(message, broadcast=True)
 
 
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
